9418a268f3a144a3d9204e997e38fd69ec28d449
“yang-samuel.lu” <yang-samuel.lu@schneider-electric.com>
2017-11-21 10:25:58 +0800

    adjust some code

--- run.sh
+++ run.sh
@@ -4,8 +4,6 @@ rm ../build_file/*.apk
 
 cp ./android/customModules/ShareModule.java ./node_modules/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/share/ShareModule.java
 
-
-export https_proxy=http://101.231.121.17:80
 # rm ../android/local.properties
 source ./configEnvScripts/findAndReplace.sh && getFileAndChangeJcenter && changeJSCHeader && changeBuildVersion
 #手动修改buildToolVision
